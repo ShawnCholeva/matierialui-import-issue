ba456de4c8ad6b511d7f9b821164e6b2e6378836
ShawnCholeva <shawncholeva@gmail.com>
2018-06-11 18:16:26 -0500

    Refactors

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from './home/home';
+import Home from "./home/home";
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route exact path='/' component={Home}/>
+                    <Route exact path="/" component={Home}/>
                 </Switch>
             </Router>
         );
--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-class Home extends Component<any, any> {
+class Home extends Component {
     render() {
         return (
           <div>
-            <AppBar position='fixed'>
+            <AppBar position="fixed">
               <Toolbar>
-                <IconButton aria-label='Menu'>
+                <IconButton aria-label="Menu">
                   <MenuIcon />
                 </IconButton>
-                <Typography variant='title' color='inherit'>Test</Typography>
+                <Typography variant="title" color="inherit">Test</Typography>
               </Toolbar>
             </AppBar>
           </div>
